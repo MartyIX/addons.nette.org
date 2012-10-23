ca5b3e64b9103cab937f70b8279de9a6a9b4901e
Patrik Votocek <patrik@votocek.cz>
2012-10-23 19:13:45 +0200

    tests libs now by composer

--- tests/index.php
+++ tests/index.php
@@ -2,9 +2,7 @@
 
 require_once __DIR__ . '/bootstrap.php';
 
-require_once LIBS_DIR . '/HttpPHPUnit/init.php';
-
-$http = new HttpPHPUnit\Main(LIBS_DIR . '/PHPUnit');
+$http = new HttpPHPUnit\Main();
 
 $cvg = $http->coverage(__DIR__ . '/../app', __DIR__ . '/coverage');
 if ($cvg) {
